0b2974ead25d10d0680e711a62d5fd8ba28ef25c
Terry <rtzchay@gmail.com>
2023-07-25 15:00:01 +0630

    new Hee

---
+++ GameReview_Project/GameReview_Project/GameReview_Project/Features/Home/Game.cs
@@ -0,0 +1,16 @@
+﻿namespace GameReview_Project.Features.Home
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Image { get; set; } = "";
+        public string Name { get; set; } = "";
+        public string Rating { get; set; } = "";
+        public string Platforms { get; set; } = "";
+        public string Description { get; set; } = "";
+        public string Review { get; set; } = "";
+ 
+        //public IEnumerable<RouteInstruction> Route { get; set; } = Array.Empty<RouteInstruction>();
+    }
+
+}
